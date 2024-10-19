1cd66b3816020b759d106c22f84e40e8eaa1b65d
Samantha Hiles <samih11@att.net>
2024-10-19 10:22:31 -0500

    Initial Commit

---
+++ Final Grade Calculator/Final_Grade_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Final_Grade_CalculatorApp.swift
+//  Final Grade Calculator
+//
+//  Created by Samantha Hiles on 9/25/24.
+//
+
+import SwiftUI
+
+@main
+struct Final_Grade_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
